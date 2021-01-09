f5da128b0aa477f0c941009389d2fdbb4c04abc5
james-pollock <jamespollock.dev@gmail.com>
2021-01-09 12:41:50 -0600

    removed empty brackets from useEffect

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ export default function App() {
   const { get } = useFetch("http://jservice.io/api/");
 
   useEffect(() => {
-    getRandom();
-  }, []);
+    return()=>{
+      getRandom();
+    }
+  });
 
   // document.addEventListener('click',handleNewQuestionClick)
   function getRandom() {
